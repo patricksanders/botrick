cec0501ba1d7e0c716f776283f1f1aec87f331b4
Patrick Sanders <mail@patricksanders.net>
2016-06-30 19:43:05 -0400

    change things and make web

--- botrick.py
+++ botrick.py
@@ -1,89 +1,19 @@
-from pymongo import MongoClient
-import random
+from wsgiref.simple_server import make_server
+from markov import Markov
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+markov = Markov()
 
 
-class Markov(object):
-        
-    def __init__(self):
-        self.collection = self.setup_db()
+def get_botrick(request):
+    return Response(markov.generate_text(500))
 
-    def add_blob(self, text):
-        """
-        Add a blob of text to the markov corpus database.
-
-        :param text: string
-        """
-        tokens = text.lower().split()
-        for i, t in enumerate(tokens):
-            try:
-                self.update_db(t, tokens[i+1])
-            except IndexError:
-                break
-
-    def generate_text(self, length=20):
-        """
-        Generate a snippet of text with a maximum of `length` words.
-
-        :param length: int
-        :return: string
-        """
-        result = []
-        current = self.get_random_record()
-        result.append(current['word'])
-        for x in range(length):
-            current = self.get_random_record(current)
-            if current:
-                result.append(current['word'])
-            else:
-                break
-
-        return ' '.join(result)
-
-    def get_random_record(self, seed=None):
-        """
-        Retrieve a random entry from markov collection.
-
-        :param seed: pymongo markov document
-        :return: pymongo markov document
-        """
-        if not seed:
-            count = self.collection.count()
-            return self.collection.find()[random.randrange(count)]
-        else:
-            word = random.choice(seed['children'])
-            return self.collection.find_one({'word': word})
-
-    def get_record(self, word):
-        """
-        Retrieve word record from markov collection.
-        :param word: string
-        :return: pymongo markov document
-        """
-        return self.collection.find_one({'word': word})
-
-    def setup_db(self):
-        """
-        Set up database connection.
-        :return: pymongo collection
-        """
-        client = MongoClient()
-        db = client.botrick
-        return db.markov
-
-    def update_db(self, word, new_child):
-        """
-        Add words to markov collection using `upsert` methodology.
-        :param word: string
-        :param new_child: string
-        """
-        self.collection.update_one({'word': word}, {'$push': {'children': new_child}}, upsert=True)
 
 if __name__ == '__main__':
-    markov = Markov()
-
-    #with open('patrick.txt') as f:
-    #    for line in f:
-    #        markov.add_blob(line)
-
-    print markov.generate_text(length=500)
-
+    config = Configurator()
+    config.add_route('patrick', '/patrick')
+    config.add_view(get_botrick, route_name='patrick')
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 8080, app)
+    server.serve_forever()
---
+++ markov.py
@@ -0,0 +1,78 @@
+from pymongo import MongoClient
+import random
+
+
+class Markov(object):
+    def __init__(self):
+        self.collection = self.setup_db()
+
+    def add_blob(self, text):
+        """
+        Add a blob of text to the markov corpus database.
+
+        :param text: string
+        """
+        tokens = text.lower().split()
+        for i, t in enumerate(tokens):
+            try:
+                self.update_db(t, tokens[i + 1])
+            except IndexError:
+                break
+
+    def generate_text(self, length=20):
+        """
+        Generate a snippet of text with a maximum of `length` words.
+
+        :param length: int
+        :return: string
+        """
+        result = []
+        current = self.get_random_record()
+        result.append(current['word'])
+        for x in range(length):
+            current = self.get_random_record(current)
+            if current:
+                result.append(current['word'])
+            else:
+                break
+
+        return ' '.join(result)
+
+    def get_random_record(self, seed=None):
+        """
+        Retrieve a random entry from markov collection.
+
+        :param seed: pymongo markov document
+        :return: pymongo markov document
+        """
+        if not seed:
+            count = self.collection.count()
+            return self.collection.find()[random.randrange(count)]
+        else:
+            word = random.choice(seed['children'])
+            return self.collection.find_one({'word': word})
+
+    def get_record(self, word):
+        """
+        Retrieve word record from markov collection.
+        :param word: string
+        :return: pymongo markov document
+        """
+        return self.collection.find_one({'word': word})
+
+    def setup_db(self):
+        """
+        Set up database connection.
+        :return: pymongo collection
+        """
+        client = MongoClient()
+        db = client.botrick
+        return db.markov
+
+    def update_db(self, word, new_child):
+        """
+        Add words to markov collection using `upsert` methodology.
+        :param word: string
+        :param new_child: string
+        """
+        self.collection.update_one({'word': word}, {'$push': {'children': new_child}}, upsert=True)
